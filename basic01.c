6e989adc86f0e53938b9a53bd986cf35ccf68a6d
lbiro <biro89laszlo@gmail.com>
2017-11-20 16:44:47 +0100

    Write basic exercise 1, 2, 3
    I wrote the first 3 basic exercises.

---
+++ basic01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+        printf("Name   : Pawn, White\n");
+        printf("DOB    : August 11, 532\n");
+        printf("Mobile : 12-3456789102\n"); 
+        return 0;
+}
