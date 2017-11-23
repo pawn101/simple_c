1560baf092f69facddd2373f97d0b3928fb71388
lbiro <biro89laszlo@gmail.com>
2017-11-23 15:41:50 +0100

    Write basic exercise 9-10
    These two exercises differ rather slightly. Both require two integers
    from the user. The first calculates their sum, the second one their
    product. They were really easy to write one after the other.

---
+++ basic09.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int a;
+        int b;
+        printf("Input the first integer: ");
+        scanf("%d", &a);
+        printf("Input the second integer: ");
+        scanf("%d", &b);
+        printf("Sum of the above two integers: %d\n", a + b);
+        return 0;
+}
