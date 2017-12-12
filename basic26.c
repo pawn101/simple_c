76b175d88da30cdee1f6c2758d4b06573ee2f7c9
lbiro <biro89laszlo@gmail.com>
2017-12-12 15:38:26 +0100

    Create basic26.c
    basic26.c: This one was so easy (one variable, one loop, no conditions)
    that it could have been the first of the basic exercises.

---
+++ basic26.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int k = 1;
+        for (k = 1; k < 26; k++) {
+                printf("%d ", 2*k);
+        }
+        printf("\n");
+        return 0;
+}
