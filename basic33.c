33a1ad167d7bf7eec0fc2c62e1c1fa910fe1cdd4
lbiro <biro89laszlo@gmail.com>
2018-01-04 15:27:19 +0100

    Create basic33.c
    basic33.c: The input reading part is the same as in some of the earlier
    tasks, e.g. basic29.c. So it differs from the specification a bit, but I
    think it is OK.

---
+++ basic33.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void readinput(int vec[5])
+{
+        char name[5][10] = {"first", "second", "third", "fourth",
+                "fifth"};
+        int k = 0;
+        for (k = 0; k < 5; k++) {
+                printf("Input the %s number: ", name[k]);
+                scanf("%d", &vec[k]);
+        }
+        return;
+}
+
+int main(void)
+{
+        int k = 0;
+        int vec[5] = {0,0,0,0,0};
+        int max = 0;
+        int k_max = 0;
+
+        readinput(vec);
+        for (k = 0; k < 5; k++) {
+                if (vec[k] > max) {
+                        max = vec[k];
+                        k_max = k + 1;
+                }
+        }
+        printf("Highest value: %d\n", max);
+        printf("Position: %d\n", k_max);
+        return 0;
+}
