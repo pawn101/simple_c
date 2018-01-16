612163904a673022b520e7b07595b38d916fadd6
lbiro <biro89laszlo@gmail.com>
2018-01-16 11:48:53 +0100

    Manually restore basic48.c
    basic48.c: I checked it after the last commit and found that I have not
    restored it to the original. So now I rewrote the code.

--- basic48.c
+++ basic48.c
@@ -18,8 +18,12 @@ int main(void)
         int vec[5] = {0,0,0,0,0};
         readinput(vec);
         for (k = 0; k < 5; k++) {
-                if (vec[k] < 5) {
-                        printf("n[%d] = %d", k, vec[k]);
+                printf("n[%d] = ", k);
+                if (vec[k] <= 0) {
+                        printf("100\n");
+                }
+                else {
+                        printf("%d\n", vec[k]);
                 }
         }
         return 0;
